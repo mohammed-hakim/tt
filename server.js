4262cc857e455f78adfbf6904953d13afcfcc5b3
hakimdev <devhakimpro88@gmail.com>
2022-07-12 16:52:09 +0100

    update

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ app.use(session({
         maxAge: 1000 * 60 * 60 * 24 * 30 * 12 * 10,
         httpOnly: !true, //not access in browser
         sameSite: false, //'lax', //csrf
-        secure: true //__prod__, //https
+        secure: false //__prod__, //https
     }
 }))
 
